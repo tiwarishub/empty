ca57a58c6d3a3216999980bf86f59eebd7d1eb4d
Shubham Tiwari <callst1994@gmail.com>
2022-10-17 16:16:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import os
 
 ACTIONS_RUNTIME_TOKEN = os.environ.get("ACTIONS_RUNTIME_TOKEN")
 CACHE_URL = os.environ.get("CACHE_URL")
-print(CACHE_URL)
+print("cache url",CACHE_URL)
 
 response = requests.get(url = CACHE_URL, headers = {"Authorization": "Bearer " + ACTIONS_RUNTIME_TOKEN})
 
