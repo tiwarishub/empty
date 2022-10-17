1fd808577483e8d3f702957e02388bf89002b748
Shubham Tiwari <64764738+tiwarishub@users.noreply.github.com>
2022-10-17 15:29:45 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+import requests
+import os
+
+ACTIONS_RUNTIME_TOKEN = os.environ.get("ACTIONS_RUNTIME_TOKEN")
+CACHE_URL = os.environ.get("CACHE_URL")
+print(CACHE_URL)
+
+response = requests.get(url = CACHE_URL, headers = {"Authorization": "Bearer " + ACTIONS_RUNTIME_TOKEN})
+
+print(response.json())
+
+archiveLocation = response.json()["archiveLocation"]
+print("Archive location", archiveLocation)
+
+response2 = requests.get(url = archiveLocation)
+
+
